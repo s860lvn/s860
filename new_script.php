7f73de072fad0a3e12f08c6bd559f28f18446a15
vvaleriy <valeriy.v@starkey.co.il>
2021-01-12 08:43:17 +0200

    test

--- new_script.php
+++ new_script.php
@@ -2,6 +2,9 @@
 echo "Hello GITHUB!";
 echo "New string added!";
 echo "added one more string";
+echo "added one more string in MASTER(1str)";
+echo "added one more string in MASTER(2str)";
+echo "added one more string in MASTER(3str)";
 echo "added one more string in new_branch2-1";
 echo "added one more string in new_branch2-2";
 echo "added one more string in new_branch2-3";
