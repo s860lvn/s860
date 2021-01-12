33f32ad5fd502e96d268efd681d76355b3a78e84
vvaleriy <valeriy.v@starkey.co.il>
2021-01-12 09:39:38 +0200

    xxx

--- new_script.php
+++ new_script.php
@@ -8,4 +8,5 @@ echo "added one more string in MASTER(3str)";
 echo "added one more string in new_branch2-1";
 echo "added one more string in new_branch2-2";
 echo "added one more string in new_branch2-3";
+echo "XYU";
 ?>
